a5a7b15253bae523f627d7460dafcaf09b8cc135
Mike <szekleym@gmail.com>
2019-01-23 09:40:07 -0700

    fixed bootstrap and added basic elements to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      Hello World!
+          Hello World!
+        <h1>JS Flashcards</h1>
+        <div className="row">
+          <div className="col-sm-1">test</div>
+          <div className="card container">
+            <div className="card card-title">
+              Card Title
+            </div>
+            <div className="card card-body col-lg-6">
+              Card text goes here!
+            </div>
+          </div>
+          <div className="col-sm-1">test</div>
+        </div>
+        <input type="text" placeholder="Type here"></input>
+        <button type="submit">Submit</button>
       </div>
     );
   }
